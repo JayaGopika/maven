f12cc88c6a1941c2e786e18c36f524d550548a4b
JayaGopika <168330472+JayaGopika@users.noreply.github.com>
2025-04-23 14:30:53 +0530

    Add files via upload

---
+++ mavenproject1/src/main/java/com/sxcce/mavenproject1/Mavenproject1.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.sxcce.mavenproject1;
+
+/**
+ *
+ * @author IT20
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        int a= 10;
+        int b=20;
+        int add=a+b;
+        int sub=a-b;
+        int multi= a*b;
+        int div=a/b;
+        System.out.println("Addition:" + add);
+        System.out.println("Subraction:" + sub);
+        System.out.println("Multiplication:" + multi);
+        System.out.println("divition:" + div);
+    }
+}
